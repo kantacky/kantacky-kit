97d8853ebed654c8001c1f982585599018407ea9
Kanta Oikawa <iam@kantacky.com>
2025-10-16 22:30:34 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "kantacky-kit",
+    platforms: [
+        .iOS(.v26),
+        .macCatalyst(.v26),
+        .macOS(.v26),
+        .tvOS(.v26),
+        .visionOS(.v26),
+        .watchOS(.v26),
+    ],
+    products: [
+    ],
+    targets: [
+    ]
+)
