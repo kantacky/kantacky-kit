416480651368a828d232f2a9d92197b5d597d166
Kanta Oikawa <iam@kantacky.com>
2025-10-19 00:22:29 +0900

    Add HeadphoneMotionManager

---
+++ Sources/HeadphoneMotion/HeadphoneMotionManager.swift
@@ -0,0 +1,42 @@
+//
+//  HeadphoneMotionManager.swift
+//  kantacky-kit
+//
+//  Created by Kanta Oikawa on 2025/10/19.
+//
+
+@preconcurrency import CoreMotion
+
+public final class HeadphoneMotionManager: NSObject, CMHeadphoneMotionManagerDelegate, Sendable {
+    private let manager: CMHeadphoneMotionManager
+
+    nonisolated(unsafe) public private(set) var isConnectedStream: AsyncStream<Bool>
+    nonisolated(unsafe) private var isConnectedContinuation: AsyncStream<Bool>.Continuation?
+
+    public override init() {
+        self.manager = .init()
+        self.isConnectedStream = AsyncStream { _ in }
+        super.init()
+        manager.delegate = self
+        self.isConnectedStream = AsyncStream { continuation in
+            self.isConnectedContinuation = continuation
+            continuation.onTermination = { [weak self] _ in
+                self?.manager.stopConnectionStatusUpdates()
+            }
+        }
+        manager.startConnectionStatusUpdates()
+    }
+
+    deinit {
+        isConnectedContinuation?.finish()
+        manager.stopConnectionStatusUpdates()
+    }
+
+    public func headphoneMotionManagerDidConnect(_ manager: CMHeadphoneMotionManager) {
+        isConnectedContinuation?.yield(true)
+    }
+
+    public func headphoneMotionManagerDidDisconnect(_ manager: CMHeadphoneMotionManager) {
+        isConnectedContinuation?.yield(false)
+    }
+}
