12a00c6749558889676cb83d9b5a91ee84c7b9ab
Kanta Oikawa <iam@kantacky.com>
2025-11-06 15:08:19 +0900

    Add LiveCapture app

---
+++ Examples/LiveCapture/LiveCapture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveCapture
+//
+//  Created by Kanta Oikawa on 2025/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/LiveCapture/LiveCapture/LiveCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCaptureApp.swift
+//  LiveCapture
+//
+//  Created by Kanta Oikawa on 2025/11/06.
+//
+
+import SwiftUI
+
+@main
+struct LiveCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
